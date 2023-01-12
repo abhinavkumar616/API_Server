a813cd36809e44d46fe2a199813c1c9a16ff636a
Abhinav <abhinavkumar616@gmail.com>
2023-01-12 22:03:52 +0530

    first commit

---
+++ dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+async function getConnect(){
+    try{
+        await mongoose.connect(process.env.DBKEY)
+        console.log("Database is Connected");
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+getConnect()
